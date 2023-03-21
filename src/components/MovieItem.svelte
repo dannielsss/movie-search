5a1408abf5df325a9474c1a47765365d2a8df78e
dannielsss <gamekik92@gmail.com>
2023-03-21 13:44:03 -0600

    add poster not found

--- src/components/MovieItem.svelte
+++ src/components/MovieItem.svelte
@@ -1,11 +1,19 @@
 <script>
+  import ImageNotFoundSymbol from '../assets/image-notfund-symbol.svg';
+
   export let title;
   export let poster;
   export let year;
 </script>
 
 <div>
-  <img src={poster} alt="" class="poster" />
+  {#if poster === 'N/A'}
+    <div class="poster poster-notfound">
+      <img src={ImageNotFoundSymbol} alt="" /> Poster not found
+    </div>
+  {:else}
+    <img src={poster} alt="" class="poster" />
+  {/if}
   <div class="title">
     <p>{title}</p>
     <span>{year}</span>
@@ -20,7 +28,24 @@
     border-radius: 5px;
   }
 
+  .poster-notfound {
+    background: #090c10;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+    font-weight: 300;
+    font-size: 18px;
+  }
+
+  .poster-notfound img {
+    width: 25px;
+  }
+
   .title {
+    margin-top: 6px;
     font-weight: 500;
     text-align: center;
   }
